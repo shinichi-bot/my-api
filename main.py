21f3c0eca41771de843aba4c00562d0b5b4e3c7f
shinichi <ss.service.break@gmail.com>
2026-02-18 16:22:52 +0900

    add CORS middleware

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 CONTENTS = {
     "A": ["contents_1", "contents_2", "contents_3"],
